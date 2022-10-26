2c721f6282843f333314d8f3313d447509d5e942
AlexisNogueraKurt <ale.capo.9634@gmail.com>
2022-10-26 11:41:32 -0300

    avances

--- Api/app.js
+++ Api/app.js
@@ -11,9 +11,7 @@ app.use(express.json())
 const port = 3000
 
 
-
 app.get('/api/camisetas', (req, res) => {
-
     let data = fs.readFileSync('../Datos/Camisetas.json');
     let camisetas = JSON.parse(data);
     console.log(camisetas);
