8f93fd6a7a8378184715c0c764f5c45e90178641
AlexisNogueraKurt <ale.capo.9634@gmail.com>
2022-10-26 11:23:33 -0300

    por fin, siuuuuuu!!!! avance api de la app

--- Api/app.js
+++ Api/app.js
@@ -13,8 +13,11 @@ const port = 3000
 
 
 app.get('/api/camisetas', (req, res) => {
-    let()
-    res.send('Camisetas.json')
+
+    let data = fs.readFileSync('../Datos/Camisetas.json');
+    let camisetas = JSON.parse(data);
+    console.log(camisetas);
+    res.send(camisetas);
 })
 
 
