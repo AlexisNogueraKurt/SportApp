aa52476ed3046866e5c42387a33d3bec16a6adbd
AlexisNogueraKurt <ale.capo.9634@gmail.com>
2022-11-04 14:46:01 -0300

    a

--- js/camisetas.js
+++ js/camisetas.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             for (let i = 0; i < camisetas.length; i++) {
                 tarjetasCamisetas += `
-                    <div class="column is-one-fifth-desktop is-full-mobile">
+                    <div class="column is-half-mobile is-one-quarter-desktop">
                         <div class="card">
                             <header class="card-header">
                                 <p class="card-header-title">
